94fc3c987df5a5580c163e14a35507fc8ee37b68
dheerajr17 <dheerajr17@gmail.com>
2016-02-18 19:45:37 +0530

    changed set alarm activity

--- app/src/main/java/dp/com/medtime/SetAlarmActivity.java
+++ app/src/main/java/dp/com/medtime/SetAlarmActivity.java
@@ -6,17 +6,59 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class SetAlarmActivity extends AppCompatActivity {
+import info.hoang8f.widget.FButton;
 
+public class SetAlarmActivity extends AppCompatActivity implements View.OnClickListener{
+    FButton setAlarm;
+    RadioButton morning,afternoon,night;
+    EditText medName,medDays;
+    EditText dosage1,dosage2,dosage3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        setAlarm=(FButton)findViewById(R.id.setAlarm);
+        morning=(RadioButton)findViewById(R.id.morning);
+        afternoon=(RadioButton)findViewById(R.id.afternoon);
+        night=(RadioButton)findViewById(R.id.night);
+        medName=(EditText)findViewById(R.id.medName);
+        medDays=(EditText)findViewById(R.id.medDays);
+        dosage1=(EditText)findViewById(R.id.dosage1);
+        dosage2=(EditText)findViewById(R.id.dosage2);
+        dosage3=(EditText)findViewById(R.id.dosage3);
+        dosage1.setEnabled(false);
+        dosage2.setEnabled(false);
+        dosage3.setEnabled(false);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.setAlarm:
+                break;
+            case R.id.morning:
+                dosage1.setEnabled(true);
+                dosage1.setText("");
+                break;
+            case R.id.afternoon:
+                dosage2.setEnabled(true);
+                dosage2.setText("");
+                break;
+            case R.id.night:
+                dosage3.setEnabled(true);
+                dosage3.setText("");
+                break;
+            case R.id.medName:
+                medName.setText("");
+                break;
+            case R.id.medDays:
+                medDays.setText("");
+                break;
+        }
+    }
 }
